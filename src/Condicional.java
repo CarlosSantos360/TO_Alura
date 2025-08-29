17c4b580c42cd86a74b6da038db1c4aaab36b5c4
Carlos Jose <carlosjosewipro@hotmail.com>
2025-08-29 00:06:10 -0300

    Planos de Filmes

---
+++ src/Condicional.java
@@ -0,0 +1,19 @@
+public class Condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 1990;
+        boolean incluidoNoPlano  = false;
+        double notaDoFilme = 8.1;
+        String tipoDoPlano = "plus";
+
+        if (anoDeLancamento >= 2022){
+            System.out.println("Lançamentos que os clientes estão curtindo!");
+        } else {
+            System.out.println("Filme Retrô que vale a pena assistir!");
+        }
+        if (incluidoNoPlano == true && tipoDoPlano .equals("plus")) {
+            System.out.println("Filme Liberado");   ;
+        }else {
+            System.out.println("Filme precisa ser alugado");
+        }
+    }
+}
