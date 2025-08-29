4694be07427cc45f6fc0276b55464d986e8d0c3f
Carlos Jose <carlosjosewipro@hotmail.com>
2025-08-28 23:29:10 -0300

    Aula 1

---
+++ src/Atividade1.java
@@ -0,0 +1,14 @@
+public class Atividade1 {
+    public static void main(String[] args) {
+        int celsius = 33;
+        double conversor = (celsius * 1.8) + 32;
+
+        String temperatura;
+        temperatura = """
+                A temperatura em graus Celsius é %d° 
+                E quando convertida  para Fahrenheit fica %.2f° 
+                """.formatted(celsius,conversor);
+        System.out.println(temperatura);
+    }
+
+}
